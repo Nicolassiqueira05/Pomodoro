920c8e69955d5f2940155e97617576bfb1b7a174
Nicolas Siqueira <nicolas2906ss@gmail.com>
2022-10-04 10:04:33 -0300

    sound added and more code reduction

--- src/components/patterns/Counter.jsx
+++ src/components/patterns/Counter.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import useSound from 'use-sound'
+import bells from '../../sounds/Bells.mp3'
 import Timer from './Timer'
 import Button from '../atoms/Button'
 
@@ -7,25 +9,30 @@ const Counter = (props) =>{
     const [isPaused, setIsPaused] = useState(true);
     const [isWorking, setIsWorking] = useState(false);
     const [hasLeftzero, setHasLeftzero] = useState(null)
-    const [minutes, setMinutes] = useState(25);
-    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(2);
+
+    const [play] = useSound(bells)
 
     if(seconds === -1){
         setSeconds(59)
         setMinutes(minutes - 1)
     }
 
+    function setTime(minutes, seconds, isWorking){
+        setMinutes(minutes)
+        setSeconds(seconds)
+        setIsWorking(isWorking)
+        play()
+    }
+
     if(minutes === 0 && seconds === 0){
         switch(isWorking) {
             case true:
-                setMinutes(5)
-                setSeconds(0)
-                setIsWorking(false)
+                setTime(5, 0, false)
                 break
             case false:
-                setMinutes(25)
-                setSeconds(0)
-                setIsWorking(true)
+                setTime(25, 0, false)
         }
     }
 
